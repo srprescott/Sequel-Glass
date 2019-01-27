e60379b63f0510dcade1f90367e69e534fb27672
S R Prescott <sararprescott@yahoo.com>
2019-01-27 00:24:50 -0500

    Home Page Draft 1 Complete

--- contactform.php
+++ contactform.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit'])) {
     
     mail($mailTo, $subject, $txt, $headers);
     
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }
 
 ?>
